ddfe3aa2541a27ad5f7c82af939491d04b89bc81
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-05-05 16:47:14 +0200

    [setup] project files & folder structure

---
+++ front-end/src/app/views/authenticated/authenticated.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authenticated',
+  imports: [],
+  templateUrl: './authenticated.component.html',
+  styleUrl: './authenticated.component.scss'
+})
+export class AuthenticatedComponent {
+
+}
