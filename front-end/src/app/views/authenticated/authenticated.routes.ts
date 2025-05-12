1a66263e49875fdeaa7fccdef9d592f7f12dcd12
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-05-12 07:47:59 +0300

    style the header & language drop down

--- front-end/src/app/views/authenticated/authenticated.routes.ts
+++ front-end/src/app/views/authenticated/authenticated.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AuthenticatedComponent } from './authenticated.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthenticatedComponent,
+  },
+];
