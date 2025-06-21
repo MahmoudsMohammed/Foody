808978de04a56bb5730c0d82cb5925aa8f87d465
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-06-21 09:23:51 +0300

    [add] filter meals by tag

--- front-end/src/app/views/authenticated/features/all-meals/components/tags/tags.component.ts
+++ front-end/src/app/views/authenticated/features/all-meals/components/tags/tags.component.ts
@@ -11,6 +11,7 @@ import { AllMealsService } from '../../services/allmeals.service';
 import { Tag } from '../../models/meals.model';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { NgClass } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tags',
@@ -24,10 +25,23 @@ export class TagsComponent implements OnInit {
   selectedTag = signal<string>('All');
   tags!: Signal<Tag[] | undefined>;
   _injectionContext = inject(Injector);
+  _router = inject(Router);
+  _activateRoute = inject(ActivatedRoute);
 
   ngOnInit(): void {
     this.tags = toSignal(this._allMeals.getTags(), {
       injector: this._injectionContext,
+    });
+  }
+
+  onTagActivate(tag: Tag) {
+    this.selectedTag.set(tag.name);
+    this._router.navigate([], {
+      relativeTo: this._activateRoute,
+      queryParamsHandling: 'merge',
+      queryParams: {
+        tag: this.selectedTag(),
+      },
     });
   }
 }
